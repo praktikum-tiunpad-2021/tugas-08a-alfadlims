075bc3324ff7a3db129d91b6b2a3445cce695586
alfadlims <alfadli20001@mail.unpad.ac.id>
2021-04-14 13:20:30 +0700

    tugas8 Alfadli

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 > Materi: Queue
 > Pengajar: Asisten Praktikum 2021
-## NAMA - NPM
+## ALFADLI MAULANA SIDDIK - 140810200005
 
 **Harap untuk membaca instruksi terlebih dahulu sampai dipahami!**
 
--- queue.hpp
+++ queue.hpp
@@ -10,6 +10,9 @@ namespace priority_queue {
 template <typename T>
 struct Element {
   // Implementasikan di sini.
+  T value;
+  int priority;
+  Element* next;
 };
 
 template <typename T>
@@ -21,6 +24,8 @@ using ElementPtr = Element<T> *;
 template <typename T>
 struct Queue {
   // Implementasikan di sini.
+  ElementPtr<T> head;
+  ElementPtr<T> tail;
 };
 
 /**
@@ -31,6 +36,10 @@ struct Queue {
 template <typename T>
 Queue<T> new_queue() {
   // Implementasikan di sini.
+  Queue<T> q;
+  q.head = nullptr;
+  q.tail = nullptr;
+  return q;
 }
 
 /**
@@ -43,6 +52,35 @@ Queue<T> new_queue() {
 template <typename T>
 void enqueue(Queue<T> &q, const T &value, int priority) {
   // Implementasikan di sini.
+  ElementPtr<T> pNew = new Element<T>;
+  pNew->value = value;
+  pNew->priority = priority;
+  pNew->next = nullptr;
+
+  ElementPtr<T> pRev = nullptr;
+  ElementPtr<T> pHelp = q.head;
+  if(q.head == nullptr && q.tail == nullptr){
+    q.head = pNew;
+    q.tail = pNew;
+  }else{
+    while(pNew->priority <= pHelp->priority){
+      if(pHelp->next == nullptr)
+        break;
+      pRev = pHelp;
+      pHelp = pHelp->next;
+    }
+
+    if(pHelp == q.head && pNew->priority > pHelp->priority){
+      pNew->next = pHelp;
+      q.head = pNew;
+    }else if(pHelp == q.tail && pNew->priority < pHelp->priority){
+      pHelp->next = pNew;
+      q.tail = pNew;
+    }else{
+      pRev->next = pNew;
+      pNew->next = pHelp;
+    }
+  }
 }
 
 /**
@@ -54,6 +92,7 @@ void enqueue(Queue<T> &q, const T &value, int priority) {
 template <typename T>
 T top(const Queue<T> &q) {
   // Implementasikan di sini.
+  return q.head->value;
 }
 
 /**
@@ -64,6 +103,18 @@ T top(const Queue<T> &q) {
 template <typename T>
 void dequeue(Queue<T> &q) {
   // Implementasikan di sini.
+  ElementPtr<T> pDel = nullptr;
+  if(q.head == nullptr && q.tail == nullptr){
+    std::cout << "Queue Kosong" << std::endl;
+  }else if(q.head->next == nullptr){
+    pDel = q.head;
+    q.head = nullptr;
+  }else{
+    pDel = q.head;
+    q.head = q.head->next;
+  }
+
+  delete pDel;
 }
 
 }  // namespace priority_queue
